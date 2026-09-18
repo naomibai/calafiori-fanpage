d9814f93f3190d11bc5837c1ea96ff9a12f86afe
naomibai <naomibai@users.noreply.github.com>
2026-09-18 20:22:13 +0800

    Fix git pull --rebase order in Actions workflow and local bat (was failing on dirty tree)

--- calafiori-social-auto.bat
+++ calafiori-social-auto.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d %~dp0
 echo ==== %date% %time% ==== >> "%USERPROFILE%\social-collect.log"
 call npm run fetch-social >> "%USERPROFILE%\social-collect.log" 2>&1
-git pull --rebase origin main >> "%USERPROFILE%\social-collect.log" 2>&1
 git add data/social.json >> "%USERPROFILE%\social-collect.log" 2>&1
 git commit -m "chore: refresh social data" >> "%USERPROFILE%\social-collect.log" 2>&1
+git pull --rebase origin main >> "%USERPROFILE%\social-collect.log" 2>&1
 git push >> "%USERPROFILE%\social-collect.log" 2>&1
